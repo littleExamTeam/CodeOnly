785b5dcbd51de3bc14e0ca9ba09f6cc452fff568
JoeyChen <1596305590@qq.com>
2021-01-02 17:43:31 +0800

    ls 0.4

--- ByteSel.v
+++ ByteSel.v
@@ -3,12 +3,16 @@
 
 module ByteSel(
     input wire [31:0] addra,
+    input wire [31:0] data,
     input wire [7:0] ALUControl,
-    output wire [3:0] sel
+    output wire [3:0] sel,
+    output wire [31:0] dataOut
 );
 
 reg [3:0] select;
+reg [3:0] dataout;
 assign sel = select;
+assign dataOut = dataout;
 
 always @(*)
 begin
@@ -20,16 +24,30 @@ begin
         `EXE_LW_OP: select <= 4'b0000;
         `EXE_SB_OP: begin
             case(addra[1:0])
-                2'b00: select <= 4'b1000;
-                2'b01: select <= 4'b0100;
-                2'b10: select <= 4'b0010;
-                2'b11: select <= 4'b0001;
+                2'b00: begin 
+                    select <= 4'b1000;
+                    dataout <= data << 2'd24;
+                end
+                2'b01: begin 
+                    select <= 4'b0100;
+                    dataout <= data << 2'd16;
+                end
+                2'b10: begin 
+                    select <= 4'b0010;
+                    dataout <= data << 1'd8;
+                end
+                2'b11: begin 
+                    select <= 4'b0001;
+                end
             endcase
         end
 
         `EXE_SH_OP: begin
             case(addra[1:0])
-                2'b00: select <= 4'b1100;
+                2'b00: begin 
+                    select <= 4'b1100;
+                    dataout <= data << 2'd16;
+                end
                 2'b10: select <= 4'b0011;
             endcase
         end
