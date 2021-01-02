0af31e8871661e00b2006408db42356f947cded4
JoeyChen <1596305590@qq.com>
2021-01-02 10:20:49 +0800

    change the branch

--- controller.v
+++ controller.v
@@ -3,20 +3,21 @@
 
 module main_dec(
     input wire [5:0] op,funct,
+    input wire [4:0] rt,
 
     output wire jump, regwrite, regdst,
-    output wire alusrcA,//正常的话�?0
+    output wire alusrcA,//正常的话�?0
     output wire [1:0] alusrcB, //这里修改成两位是为了选择操作数，00 normal 01 Sign 10 UNsign
     output wire branch, memwrite, 
-    output wire [1:0] DatatoReg,//这里是去找写到寄存器中的�? 11 mem 10 HI 01 LO 00 ALU  there need changed to 3bits for div and mult
+    output wire [1:0] DatatoReg,//这里是去找写到寄存器中的�? 11 mem 10 HI 01 LO 00 ALU  there need changed to 3bits for div and mult
     output wire HIwrite,//这里是去寻找是否写HILO 直接传给HILO
-    output wire LOwrite, //选择写的是HI还是LO寄存�?? 0 LO 1 HI  信号传给HILO
-    output wire [1:0] DataToHI, //这里是因为乘除法器加上的信号�?00选ALU 01选乘�? 10 选除�?
-    output wire [1:0] DataToLO,  //这里是因为乘除法器加上的信号�?00选ALU 01选乘�? 10 选除�?
+    output wire LOwrite, //选择写的是HI还是LO寄存�?? 0 LO 1 HI  信号传给HILO
+    output wire [1:0] DataToHI, //这里是因为乘除法器加上的信号�?00选ALU 01选乘�? 10 选除�?
+    output wire [1:0] DataToLO,  //这里是因为乘除法器加上的信号�?00选ALU 01选乘�? 10 选除�?
     output wire Sign, //这个是乘除法的符号数
-    output wire startDiv, //乘除法的�?始信�?
+    output wire startDiv, //乘除法的�?始信�?
 
-    output wire annul, //乘除法取消信�?
+    output wire annul, //乘除法取消信�?
 //=====新加的关于跳转的指令=====
     output wire jal,
     output wire jr,
@@ -25,8 +26,8 @@ module main_dec(
 
 );
 
-reg [21:0] signals; //添加LOwrite之后变成11�??
-//TODO: 记得明天通路中需要修改这个位�? 12.30 晚上 12 > 13
+reg [21:0] signals; //添加LOwrite之后变成11�??
+//TODO: 记得明天通路中需要修改这个位�? 12.30 晚上 12 > 13
 
 //assign {jump, regwrite, regdst, alusrcB[1:0], branch, memwrite, DatatoReg} = signals;
 assign {regwrite, DatatoReg[1:0], memwrite, alusrcA ,{alusrcB[1:1]}, {alusrcB[0:0]}, regdst, jump, branch,
@@ -103,12 +104,23 @@ always @(*) begin
 //======ARI=============
         `EXE_BEQ: signals <= 22'b0_00_0_0_00_0_0_1_0_0_00_00_0_0_0_0_0_0;
         `EXE_BNE: signals <= 22'b0_00_0_0_00_0_0_1_0_0_00_00_0_0_0_0_0_0;
-        `EXE_BGEZ: signals <= 22'b0_00_0_0_00_0_0_1_0_0_00_00_0_0_0_0_0_0;
+
+        // `EXE_BGEZ: signals <= 22'b0_00_0_0_00_0_0_1_0_0_00_00_0_0_0_0_0_0;
+        // `EXE_BLTZ: signals <= 22'b0_00_0_0_00_0_0_1_0_0_00_00_0_0_0_0_0_0;
+        // `EXE_BGEZAL: signals <= 22'b1_00_0_0_00_0_0_1_0_0_00_00_0_0_0_0_0_1;
+        // `EXE_BLTZAL: signals <= 22'b1_00_0_0_00_0_0_1_0_0_00_00_0_0_0_0_0_1;
+        `6'b000001:begin
+            case(rt)
+                `EXE_BGE:signals <= 22'b0_00_0_0_00_0_0_1_0_0_00_00_0_0_0_0_0_0;
+                `EXE_BLTZ:signals <= 22'b0_00_0_0_00_0_0_1_0_0_00_00_0_0_0_0_0_0;
+                `EXE_BGEZAL:signals <= 22'b1_00_0_0_00_0_0_1_0_0_00_00_0_0_0_0_0_1;
+                `EXE_BLTZAL:signals <= 22'b1_00_0_0_00_0_0_1_0_0_00_00_0_0_0_0_0_1;
+            endcase
+        end
+
+
         `EXE_BGTZ: signals <= 22'b0_00_0_0_00_0_0_1_0_0_00_00_0_0_0_0_0_0;
         `EXE_BLEZ: signals <= 22'b0_00_0_0_00_0_0_1_0_0_00_00_0_0_0_0_0_0;
-        `EXE_BLTZ: signals <= 22'b0_00_0_0_00_0_0_1_0_0_00_00_0_0_0_0_0_0;
-        `EXE_BGEZAL: signals <= 22'b1_00_0_0_00_0_0_1_0_0_00_00_0_0_0_0_0_1;
-        `EXE_BLTZAL: signals <= 22'b1_00_0_0_00_0_0_1_0_0_00_00_0_0_0_0_0_1;
         `EXE_J: signals <= 22'b0_00_0_0_00_0_1_0_0_0_00_00_0_0_0_0_0_0;
         `EXE_JAL: signals <= 22'b1_00_0_0_00_0_1_0_0_0_00_00_0_0_0_1_0_0;
         
@@ -128,6 +140,7 @@ endmodule
 
 module controller(
     input wire [5:0] Op, Funct,
+    input wire [4:0] rt,
     output wire Jump, RegWrite, RegDst,
     output wire ALUSrcA, 
     output wire [1:0] ALUSrcB, 
@@ -135,12 +148,12 @@ module controller(
     output wire Branch, MemWrite, 
     output wire [1:0]DatatoReg,
     output wire HIwrite,LOwrite,
-    output wire [1:0] DataToHI, //这里是因为乘除法器加上的信号�?00选ALU 01选乘�? 10 选除�?
-    output wire [1:0] DataToLO,  //这里是因为乘除法器加上的信号�?00选ALU 01选乘�? 10 选除�?
+    output wire [1:0] DataToHI, //这里是因为乘除法器加上的信号�?00选ALU 01选乘�? 10 选除�?
+    output wire [1:0] DataToLO,  //这里是因为乘除法器加上的信号�?00选ALU 01选乘�? 10 选除�?
     output wire Sign, //这个是乘除法的符号数
-    output wire startDiv, //乘除法的�?始信�?
+    output wire startDiv, //乘除法的�?始信�?
 
-    output wire annul, //乘除法取消信�?
+    output wire annul, //乘除法取消信�?
     output wire [7:0] ALUContr,
     //=====新加的关于跳转的指令=====
     output wire jal,
@@ -154,6 +167,7 @@ module controller(
 main_dec main_dec(
     .op(Op),
     .funct(Funct),
+    .rt(rt),
     .jump(Jump),
     .regwrite(RegWrite),
     .regdst(RegDst),
